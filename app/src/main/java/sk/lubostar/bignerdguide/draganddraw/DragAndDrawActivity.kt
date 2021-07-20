914be722aa8b4bbc58de6b47080d1df8b7883acf
staracek <staracek@foundation.sk>
2021-07-20 22:23:45 +0200

    main activity rename

--- app/src/main/java/sk/lubostar/bignerdguide/draganddraw/DragAndDrawActivity.kt
+++ app/src/main/java/sk/lubostar/bignerdguide/draganddraw/DragAndDrawActivity.kt
@@ -3,7 +3,7 @@ package sk.lubostar.bignerdguide.draganddraw
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class DragAndDrawActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
